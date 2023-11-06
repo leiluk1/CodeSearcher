a5f7039d7abac9a9f917859bb24ccb634570f30c
Karina Denisova <k.denisova@innopolis.university>
2023-11-06 04:03:21 +0300

    add: dataloade and dtdset class

---
+++ src/datasets/CoDesc/make_dataset.py
@@ -0,0 +1,69 @@
+import py7zr
+import gzip
+import os.path
+from loguru import logger
+import gdown
+import pandas as pd
+from random import sample
+from ast import literal_eval
+
+
+DATA_RAW_FOLDER = 'data/raw/CoDesc_data/'
+URL = "https://drive.google.com/uc?id=1NKHb_mCH345NXcMFUBw5SxOgki8N5wsO"
+MODES = ['train', 'test', 'val']
+
+
+def retrieve_json(option='train'):
+        json_list = []
+        path = DATA_RAW_FOLDER + f"csn_preprocessed_data_balanced_partition/java/final/jsonl/{option}/"
+        for file_name in os.listdir(path):
+            file_path = os.path.join(path, file_name)
+            if file_name.endswith('.jsonl.gz'):
+                with gzip.open(file_path, 'rt', encoding='utf-8') as json_file:
+                    lines = json_file.readlines()
+                    json_list.extend(lines)
+        return json_list 
+
+def _setup_dataset():
+    logger.info(f"Downloading CoDesc from {URL}")
+    archive_path = f"{DATA_RAW_FOLDER}archive.7z"
+    gdown.download(URL, archive_path, quiet=False)
+    logger.info('Extracting files')
+    with py7zr.SevenZipFile(archive_path, mode='r') as z:
+        z.extractall(DATA_RAW_FOLDER)
+    logger.info('Extraction complete')
+
+
+
+class CoDescDataset:
+    def __init__(self, mode='train', download = False):
+        assert mode in MODES, f'Unsupported mode {mode}'
+
+        self._load_dataframe(download)
+        self.mode = mode
+        self.json_list = retrieve_json(mode)
+        self.df = self._process_dataframe()
+
+
+    def _load_dataframe(self, download=False):
+        if not os.path.exists(DATA_RAW_FOLDER) or download:
+            _setup_dataset()
+
+    
+
+    def _process_dataframe(self):
+        
+        sample_size = len(self.json_list)
+        dataframe = pd.DataFrame([literal_eval(line) for line in sample(self.json_list, sample_size)]).set_index('id')
+        dataframe = dataframe[['docstring_tokens', 'code_tokens']]
+
+        #rename columns
+        dataframe = dataframe.rename(columns={'docstring_tokens': 'summary'})
+        logger.info(f"Loaded {len(dataframe)} {self.mode} examples")
+        return dataframe   
+
+    def get_pandas(self):
+        return self.df
+
+if __name__ == '__main__':
+    dataset = CoDescDataset(mode='train')   
--- src/datasets/make_datasets.py
+++ src/datasets/make_datasets.py
@@ -5,6 +5,8 @@ from src.datasets.PyTorrent.make_dataset import MODES as PYTORRENT_MODES
 from src.datasets.PyTorrent.make_dataset import PyTorrentDataset
 from src.datasets.XLCoST.make_dataset import MODES as XLCOST_MODES
 from src.datasets.XLCoST.make_dataset import XLCoSTDataset
+from src.datasets.CoDesc.make_dataset import MODES as CODESC_MODES
+from src.datasets.CoDesc.make_dataset import CoDescDataset
 
 
 def create_python_dataset(max_length):
@@ -14,7 +16,9 @@ def create_python_dataset(max_length):
 
     xlcost_dataframes = [XLCoSTDataset(mode=mode, language='Python') for mode in XLCOST_MODES]
 
-    dataframes = [pd.concat([pytorrent_dataframes[i], xlcost_dataframes[i]], ignore_index=True) for i in range(3)]
+    codesc_dataframes = [CoDescDataset(mode=mode) for mode in CODESC_MODES]
+
+    dataframes = [pd.concat([pytorrent_dataframes[i], xlcost_dataframes[i], codesc_dataframes[i]], ignore_index=True) for i in range(3)]
 
     dataset['train'] = datasets.Dataset.from_pandas(dataframes[0])
     dataset['val'] = datasets.Dataset.from_pandas(dataframes[1])
