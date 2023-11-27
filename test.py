0623aaf1b0092dc9d181f6574f3d464baea1499b
Karim Galliamov <45235968+KGallyamov@users.noreply.github.com>
2023-11-27 23:48:59 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from tests.ui_test import get_decoded_text_from_model
+from tests.ui_test import get_nearest_code_from_model
 
-print(get_decoded_text_from_model('checkpoints/codet5p-220m-seq2seq/prefix-python/', 'print hello world'))
+print(get_nearest_code_from_model('checkpoints/codet5p-220m-seq2seq/prefix-python/', 'print hello world'))
