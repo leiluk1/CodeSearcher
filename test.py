e65652e7238000114beca6fc9056b5d29cd7a300
Karina Denisova <k.denisova@innopolis.university>
2023-11-27 17:06:48 +0300

    refactor : ui

---
+++ test.py
@@ -0,0 +1,3 @@
+from tests.ui_test import get_decoded_text_from_model
+
+print(get_decoded_text_from_model('checkpoints/codet5p-220m-seq2seq/prefix-python/', 'print hello world'))
--- tests/ui_test.py
+++ tests/ui_test.py
@@ -1,41 +1,20 @@
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, DataCollatorForSeq2Seq
 import torch
 from peft import PeftModel, PeftConfig
 import numpy as np
 from annoy import AnnoyIndex
 
-import sys
-sys.path.append('../')
+# import sys
+# sys.path.append('../')
+from src.fixtures import DATASET_MAP
 
-from CodeSearcher.src.datasets.make_datasets import create_python_dataset, create_java_dataset, \
-    create_csharp_dataset, create_sql_dataset, create_cpp_dataset
-
-DATASET_MAP = {'Python': create_python_dataset, 'Java': create_java_dataset,
-               'Csharp': create_csharp_dataset, 'SQL': create_sql_dataset, 'C++': create_cpp_dataset}
-
+from src.models.train import _setup_seq2seq_dataset
 
 
 def find_nearest(embedding, index, id_map, k=1):
     nearest_ids = index.get_nns_by_vector(embedding, k)
     nearest_codes = [id_map[i] for i in nearest_ids]
     return nearest_codes
-
-
-def _setup_seq2seq_dataset(raw_dataset, tokenizer, model_max_src_length, model_max_tgt_length, num_virtual_tokens):
-    def preprocess_function(examples):
-        model_inputs = tokenizer(examples['code_tokens'], max_length=model_max_src_length - num_virtual_tokens,
-                                 padding='max_length', truncation=True)
-        summary = tokenizer(examples['summary'], max_length=model_max_tgt_length - num_virtual_tokens,
-                            padding='max_length', truncation=True)
-        summary[summary == tokenizer.pad_token_id] = -100
-        model_inputs["labels"] = summary["input_ids"].copy()
-        return model_inputs
-
-    tokenized_dataset = raw_dataset.map(preprocess_function,
-                                        batched=True,
-                                        remove_columns=raw_dataset["train"].column_names,
-                                        desc='Tokenizing dataset')
-    return tokenized_dataset
 
 
 def get_decoded_text_from_model(checkpoint_path, input_text, language='Python'):
@@ -59,9 +38,16 @@ def get_decoded_text_from_model(checkpoint_path, input_text, language='Python'):
     else:
         output = model.encoder(encoded_input['input_ids']).last_hidden_state[:, 0, :]
 
-    raw_dataset = DATASET_MAP[language]  
+    raw_dataset = DATASET_MAP[language](max_length=128)
 
-    # print('raw_dataset', raw_dataset.head(5))  
+    # print('raw_dataset', raw_dataset)  
+    tokenized_dataset = _setup_seq2seq_dataset(raw_dataset, tokenizer, 128, 128, 0)
+
+    data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=config.base_model_name_or_path)
+
+    test_loader = torch.utils.data.DataLoader(tokenized_dataset['test'], batch_size=16, collate_fn=data_collator)
+
+    print('tokenized_dataset', tokenized_dataset)
 
     # Assume embeddings is a list of your code embeddings and codes is a list of the corresponding codes
     embeddings = ...
@@ -81,6 +67,6 @@ def get_decoded_text_from_model(checkpoint_path, input_text, language='Python'):
     
     return nearest_codes
 
-checkpoint_path = "checkpoints/codet5p-220m-seq2seq/prefix-python/"
-input_text = "print hello world"
-decoded_text = get_decoded_text_from_model(checkpoint_path, input_text)
+# checkpoint_path = "checkpoints/codet5p-220m-seq2seq/prefix-python/"
+# input_text = "print hello world"
+# decoded_text = get_decoded_text_from_model(checkpoint_path, input_text)
