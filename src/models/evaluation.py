26f3f2c66719563f5f12321ba15b4776726b34d1
Karim Galliamov <45235968+KGallyamov@users.noreply.github.com>
2023-11-28 11:25:47 +0300

    refactoring

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -28,7 +28,8 @@ def evaluation(model, test_loader, device, desc='Test set MRR = ', max_batches=f
     test_code_embeddings = []
     model_type = str(type(model)).lower()
     is_encoder_only = '220m-bimodal' not in model_type and 'seq2seq' not in model_type
-    for i, batch in tqdm(enumerate(test_loader), total=len(test_loader), desc='Testing the model', position=0):
+    for i, batch in tqdm(enumerate(test_loader), total=min(len(test_loader), max_batches),
+                         desc='Testing the model', position=0):
         batch.to(device)
         text_batch = batch.pop("labels")
 
