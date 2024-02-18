2555ec990e0298d9fb6c278c152ed017c71bff29
Karim Galliamov <45235968+KGallyamov@users.noreply.github.com>
2024-02-18 19:20:28 +0300

    CSN dataset integrated

--- src/datasets/make_datasets.py
+++ src/datasets/make_datasets.py
@@ -3,12 +3,24 @@ import pandas as pd
 
 from src.datasets.CoDesc.make_dataset import CoDescDataset
 from src.datasets.CoDesc.make_dataset import MODES as CODESC_MODES
+from src.datasets.CodeSearchNet.make_dataset import CodeSearchNet
+from src.datasets.CodeSearchNet.make_dataset import MODES as CSN_MODES
 from src.datasets.PyTorrent.make_dataset import MODES as PYTORRENT_MODES
 from src.datasets.PyTorrent.make_dataset import PyTorrentDataset
 from src.datasets.StaQC.make_dataset import MODES as STAQC_MODES
 from src.datasets.StaQC.make_dataset import StaQCDataset
 from src.datasets.XLCoST.make_dataset import MODES as XLCOST_MODES
 from src.datasets.XLCoST.make_dataset import XLCoSTDataset
+
+
+def create_csn_dataset(language, **kwargs):
+    dataset = datasets.DatasetDict()
+    dataframes = [CodeSearchNet(mode, language).get_pandas() for mode in CSN_MODES]
+
+    dataset['train'] = datasets.Dataset.from_pandas(dataframes[0])
+    dataset['val'] = datasets.Dataset.from_pandas(dataframes[1])
+    dataset['test'] = datasets.Dataset.from_pandas(dataframes[2])
+    return dataset
 
 
 def create_python_dataset(max_length):
